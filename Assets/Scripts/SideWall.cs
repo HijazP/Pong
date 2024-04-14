26db5cda28ff3f9298e5a18823bb27ce97e5866e
Hanif Ali Ramdani <hanifaliramdani98@gmail.com>
2024-04-14 13:03:00 +0700

    reset git

---
+++ Assets/Scripts/SideWall.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWall : MonoBehaviour
+{
+    [SerializeField]
+    private GameManager gameManager;
+    public PlayerControl player;
+
+    void OnTriggerEnter2D(Collider2D anotherCollider)
+    {
+        if (anotherCollider.name == "Ball")
+        {
+            player.IncrementScore();
+            if (player.Score < gameManager.maxScore)
+            {
+                anotherCollider.gameObject.SendMessage("RestartGame", 2.0f, SendMessageOptions.RequireReceiver);
+            }
+        }
+    }
+}
